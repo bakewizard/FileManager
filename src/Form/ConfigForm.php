e3f2ef3bc8d9bc1e519a310dfad5360aa0ae1254
bakewizard <207716455+bakewizard@users.noreply.github.com>
2026-01-29 23:31:59 +0200

    Moving to CakePHP 5.3...

--- src/Form/ConfigForm.php
+++ src/Form/ConfigForm.php
@@ -45,7 +45,7 @@ class ConfigForm extends Form
      * @inheritDoc
      */
     #[Override]
-    protected function _execute(array $data): bool
+    protected function process(array $data): bool
     {
         Configure::write($data);
 
