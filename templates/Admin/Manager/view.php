d10318fb03473e46194799aa60e29fc44f6c8ffc
bakewizard <207716455+bakewizard@users.noreply.github.com>
2026-02-25 10:48:43 +0200

    fix: update script tags to use module type

--- templates/Admin/Manager/view.php
+++ templates/Admin/Manager/view.php
@@ -5,7 +5,7 @@
  * @var mixed $path
  */
 ?>
-<?= $this->Html->script('FileManager.main', ['block' => true]) ?>
+<?= $this->Html->script('FileManager.main', ['block' => true, 'type' => 'module']) ?>
 <?php $this->assign('page', __('File Manager')); ?>
 <div class="card">
     <div class="card-header">
